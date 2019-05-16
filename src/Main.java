b61c352471072b055aa3accf4389c23a6e543e71
Alex <1993alexey@gmail.com>
2019-05-15 22:08:02 -0600

    Hello to Lu from Alex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args){
+
         System.out.println("Hello, my name is Lu");
+        System.out.println("Hello, Lu, my name is Alex, and I'm gonna hack your Java app");
+        System.out.println("\nERROR: Java Linker error has occurred. Please donate some money to support Oracle");
     }
 }
